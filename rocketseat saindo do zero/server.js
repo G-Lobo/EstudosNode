11572a94461162d8c0492f45a340eaea073b33a3
Gabriel Lobo <gbrilobo@gmail.com>
2023-10-08 15:56:42 -0300

    first commit

---
+++ rocketseat saindo do zero/server.js
@@ -0,0 +1,56 @@
+import {fastify} from 'fastify';
+import {DatabaseMemory} from './database-memory.js';
+
+const server = fastify();
+const database = new DatabaseMemory();
+
+/* post */
+server.post('/videos', (request, reply) => {
+    const {title, description, duration} = request.body;
+
+    database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(201).send();
+});
+
+/* index/show(?) */
+server.get('/videos', () => {
+    const videos = database.list();
+
+    return videos;
+});
+
+
+/* edit/update */
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id;
+    const {title, description, duration } = request.body;
+
+    database.update(videoId, {
+        title,
+        description,
+        duration,
+    });
+
+return reply.status(204).send();
+});
+
+
+/* delete */
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id;
+
+    database.delete(videoId);
+
+    return reply.status(204).send();
+});
+
+
+/* port */
+server.listen({
+    port: 3333
+});
